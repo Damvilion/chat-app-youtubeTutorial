b246b945fbed733646d894dfaf36c13eafc102cd
Noel Ohaeri <noelohaeri@gmail.com>
2023-06-08 16:40:11 -0400

    Search User Functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,58 @@
 import React from "react";
+import { useState } from "react";
+import { collection, query, where, getDoc } from "firebase/firestore";
+import { db } from "../firebase-config";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDoc(q);
+
+      console.log(querySnapshot);
+
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data);
+        console.log(user);
+      });
+    } catch (err) {
+      setErr(true);
+      console.log(err.message);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="find a user" />
+        <input
+          type="text"
+          placeholder="find a user"
+          onKeyDown={handleKey}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
       </div>
+      {err && <span>User not found!</span>}
+      {user && (
+        <div className="userChat">
+          <img src={user.PhotoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
